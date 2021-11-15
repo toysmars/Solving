646372826772bafa7587d26b5d3bff3e15f6ac4f
toysmars <toysmars@gmail.com>
2021-11-15 22:36:00 +0900

    Update SuffixArraySolver.cpp

--- library/SuffixArraySolver.cpp
+++ library/SuffixArraySolver.cpp
@@ -1,103 +1,70 @@
-class Comparator {
-public:
-    Comparator(const string& s) : str(s) {}
-    bool operator()(int a, int b) {
-        return str[a] < str[b];
-    }
-private:
-    string str;
-};
+// Suffix array solver
+struct SuffixArraySolver {
+    SuffixArraySolver(const string& str): str(str), pos(str.size()), rank(str.size()), lcp(str.size()) {}
 
-// Solve suffix array using Manber Myers algorithm
-// https://dl.acm.org/citation.cfm?id=320176.320218
-class SuffixArraySolver {
-public:
-    void solve(const string& str) {
+    void solve() {
         int n = str.size();
-        pos = vector<int>(n);
-        rank = vector<int>(n);
-        
-        vector<int> count(n);
-        vector<int> next(n);
-        vector<bool> bh(n);
-        vector<bool> b2h(n);
+        vector<int> ord(n + 1);
+        vector<int> nord(n + 1);
+        vector<int> cnt(max(256, n + 1));
+        vector<int> r(n + 1);
 
-        // initial value for pos
+        // initialize pos
         for (int i = 0; i < n; ++i) {
             pos[i] = i;
-        }
-        Comparator comparator(str);
-        sort(pos.begin(), pos.end(), comparator);
-
-        // initial value for bh and b2h;
-        for (int i = 0; i < n; ++i) {
-            bh[i] = i == 0 || str[pos[i]] != str[pos[i - 1]];
-            b2h[i] = false;
+            ord[i] = str[i];
         }
 
-        // interate over h = [1, 2, 4, 8, ..., 2^k, ...]
-        for (int h = 1; h < n; h <<= 1) {
-            // buckets
-            int i = 0;
-            int j = 0;
-            int buckets = 0;
-            while (i < n) {
-                j = i + 1;
-                while (j < n && !bh[j]) {
-                    ++j;
-                }
-                i = next[i] = j;
-                ++buckets;
-            }
-            if (buckets == n) {
+        int h = 1;
+        int done = 1;
+        while (true) {
+            fill(cnt.begin(), cnt.end(), 0);
+            for (int i = 0; i < n; ++i) cnt[ord[min(i + h, n)]]++;
+            for (int i = 1; i <= n || i <= 255; ++i) cnt[i] += cnt[i - 1];
+            for (int i = n - 1; i >= 0; --i) r[--cnt[ord[min(i + h, n)]]] = i;
+            fill(cnt.begin(), cnt.end(), 0);
+            for (int i = 0; i < n; ++i) cnt[ord[i]]++;
+            for (int i = 1; i <= n || i <= 255; ++i) cnt[i] += cnt[i - 1];
+            for (int i = n - 1; i >= 0; --i) pos[--cnt[ord[r[i]]]] = r[i];
+            if (done == n) {
                 break;
             }
-            // ranks
-            for (int i = 0; i < n; i = next[i]) {
-                count[i] = 0;
-                for (int j = i; j < next[i]; ++j) {
-                    rank[pos[j]] = i ;
+            done = 1;
+            nord[pos[0]] = 1;
+            for (int i = 1; i < n; ++i) {
+                if (ord[pos[i - 1]] != ord[pos[i]] || ord[pos[i - 1] + h] != ord[pos[i] + h]) {
+                    done++;
                 }
-            }
-            // 2h based on h
-            count[rank[n - h]] += 1;
-            b2h[rank[n - h]] = true;
-            for (int i = 0; i < n; i = next[i]) {
-                for (int j = i; j < next[i]; ++j) {
-                    int d = pos[j] - h;
-                    if (d >= 0) {
-                        rank[d] += count[rank[d]]++;
-                        b2h[rank[d]] = true;
-                    }
-                }
-                for (int j = i; j < next[i]; ++j) {
-                    int d = pos[j] - h;
-                    if (d >= 0 && b2h[rank[d]]) {
-                        for (int k = rank[d] + 1; k < n && !bh[k] && b2h[k]; ++k) {
-                            b2h[k] = false;
-                        }
-                    }
-                }
+                nord[pos[i]] = done;
             }
             for (int i = 0; i < n; ++i) {
-                pos[rank[i]] = i;
-                bh[i] = bh[i] || b2h[i];
+                ord[i] = nord[i];
             }
+            h *= 2;
         }
         for (int i = 0; i < n; ++i) {
             rank[pos[i]] = i;
         }
     }
 
-    const vector<int>& getSuffixArray() {
-        return pos;
+    void solveLCP() {
+        int n = str.size();
+        int len = 0;
+        for (int i = 0; i < n; ++i) {
+            int k = rank[i];
+            if (k > 0) {
+                int j = pos[k - 1];
+                while (str[j + len] == str[i + len]) {
+                    len += 1;
+                }
+                lcp[k] = len;
+                len -= len > 0;
+            }
+        }
     }
 
-    const vector<int>& getRank() {
-        return rank;
-    }
-
-private:
     vector<int> pos;
     vector<int> rank;
+    vector<int> lcp;
+    string str;
 };

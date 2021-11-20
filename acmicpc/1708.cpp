4d5d8db8fb31db78930ca7fa8c456b9bd9b08c3a
toysmars <toysmars@gmail.com>
2021-11-20 22:29:38 +0900

    Update 1708.cpp

--- acmicpc/1708.cpp
+++ acmicpc/1708.cpp
@@ -8,31 +8,50 @@
 using namespace std;
 
 typedef long long i64;
+typedef i64 coord_t;
 
-struct Point {
-    i64 x;
-    i64 y;
+struct Point2D {
+    coord_t x;
+    coord_t y;
 };
 
-i64 ccw(const Point& a, const Point& b, const Point& c)
-{
-    return a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y;
+typedef Point2D Vec2D;
+
+Vec2D vec(Point2D a, Point2D b) {
+    return { b.x - a.x, b.y - a.y };
 }
 
-bool cmpX(const Point& a, const Point& b) {
+coord_t normSquare(Vec2D a) {
+    return a.x * a.x + a.y * a.y;
+}
+
+coord_t cross(Vec2D a, Vec2D b) {
+    return a.x * b.y - b.x * a.y;
+}
+
+int ccw(Vec2D a, Vec2D b) {
+    coord_t c = cross(a, b);
+    if (c == 0) return 0;
+    return c > 0 ? 1 : -1;
+}
+
+coord_t ccw(Point2D p, Point2D a, Point2D b) {
+    return ccw(vec(p, a), vec(p, b));
+}
+
+bool cmpX(const Point2D& a, const Point2D& b) {
     return a.x == b.x ? a.y < b.y : a.x < b.x;
 }
 
 struct CcwComparator {
-    Point base;
-    bool operator()(const Point& a, const Point& b) {
+    Point2D base;
+    bool operator()(const Point2D& a, const Point2D& b) {
         return ccw(base, a, b) > 0;
     }
-
 };
 
-vector<Point> convexHull(vector<Point>& p) {
-    vector<Point> res;
+vector<Point2D> convexHull(vector<Point2D>& p) {
+    vector<Point2D> res;
     sort(p.begin(), p.end(), cmpX);
     CcwComparator ccwCmp = { p[0] };
     stable_sort(p.begin() + 1, p.end(), ccwCmp);
@@ -40,7 +59,7 @@ vector<Point> convexHull(vector<Point>& p) {
     res.push_back(p[1]);
     for (int i = 2; i < p.size(); ++i) {
         int size = res.size();
-        while (size >= 2 && ccw(p[i], res[size-2], res[size-1]) <= 0) {
+        while (size >= 2 && ccw(p[i], res[size-1], res[size-2]) >= 0) {
             res.pop_back();
             size--;
         }

22cf64553219de5aa2cdc870c423a5d6df0bfd60
toysmars <toysmars@gmail.com>
2021-11-20 15:48:28 +0900

    Create kmp.cpp

---
+++ library/kmp.cpp
@@ -0,0 +1,47 @@
+// KMP
+// Knuth Morris Pratt
+// String matching
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef int elem_t;
+struct KMP {
+    vector<int> prefixSufix(const vector<elem_t>& pattern)
+    {
+        int n = pattern.size();
+        vector<int> pi(n, 0);
+        for (int i = 1, j = 0; i < n; ++i) {
+            while (j > 0 && pattern[i] != pattern[j]) {
+                j = pi[j-1];
+            }
+            if (pattern[i] == pattern[j]) {
+                pi[i] = ++j;
+            } else {
+                pi[i] = j;
+            }
+        }
+        return pi;
+    }
+
+    vector<int> match(const vector<elem_t>& source, const vector<elem_t>& pattern)
+    {
+        int n = source.size();
+        int m = pattern.size();
+        vector<int> pi = prefixSufix(pattern);
+        vector<int> res;
+        for (int i = 0, j = 0; i < n; ++i) {
+            while (j > 0 && source[i] != pattern[j]) {
+                j = pi[j-1];
+            }
+            if (source[i] == pattern[j]) {
+                ++j;
+                if (j == m) {
+                    res.push_back(i-m+1);
+                    j = pi[j-1];
+                }
+            }
+        }
+        return res;
+    }
+};

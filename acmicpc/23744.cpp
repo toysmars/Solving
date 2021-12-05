52698178e1fae972b01f794544fe155304aa5493
toysmars <toysmars@gmail.com>
2021-12-05 23:21:21 +0900

    Update 23744.cpp

--- acmicpc/23744.cpp
+++ acmicpc/23744.cpp
@@ -7,11 +7,18 @@
 using namespace std;
 
 typedef int val_t;
-const val_t DEFAULT = 0;
 
+struct Operations {
+    val_t (*combine)(val_t, val_t);
+    val_t (*update)(val_t, val_t);
+};
+
+// Segment tree
+// The index of the first element should be 1.
 struct SegmentTree {
-    SegmentTree(int n, val_t (*op)(val_t, val_t)) : n(n), t(4 * n + 1, DEFAULT), op(op) {}
+    SegmentTree(int n, val_t def_val, Operations ops) : n(n), def_val(def_val), t(4 * n + 1, def_val), ops(ops) {}
 
+    // Update the value of `i`th element to `x`.
     void update(int i, val_t x) {
         update(i, x, 1, 1, n + 1);
     }
@@ -21,20 +28,21 @@ struct SegmentTree {
         }
         int len = r - l;
         if (len == 1) {
-            t[idx] = x;
+            t[idx] = ops.update(t[idx], x);
             return t[idx];
         }
         val_t res1 = update(i, x, idx * 2 + 0, l, l + len / 2);
         val_t res2 = update(i, x, idx * 2 + 1, l + len / 2, r);
-        t[idx] = op(res1, res2);
+        t[idx] = ops.combine(res1, res2);
         return t[idx];
     }
+    // Query the value elements in range [ql, qr).
     val_t query(int ql, int qr) {
         return query(ql, qr, 1, 1, n + 1);
     }
     val_t query(int ql, int qr, int idx, int l, int r) {
         if (l >= qr || r <= ql || l >= r) {
-            return DEFAULT;
+            return def_val;
         }
         if (ql <= l && qr >= r) {
             return t[idx];
@@ -42,18 +50,19 @@ struct SegmentTree {
         int len = r - l;
         val_t res1 = query(ql, qr, idx * 2 + 0, l, l + len / 2);
         val_t res2 = query(ql, qr, idx * 2 + 1, l + len / 2, r);
-        return op(res1, res2);
+        return ops.combine(res1, res2);
     }
 
     static val_t min(val_t a, val_t b) { return a < b ? a : b; }
     static val_t max(val_t a, val_t b) { return a > b ? a : b; }
+    static val_t sum(val_t a, val_t b) { return a + b; }
+    static val_t assign(val_t a, val_t b) { return b; }
 
     int n;
+    val_t def_val;
     vector<val_t> t;
-    val_t (*op)(val_t, val_t);
+    Operations ops;
 };
-
-int maxop(int a, int b) { return a > b ? a : b; }
 
 int main() {
     int n;
@@ -73,7 +82,7 @@ int main() {
     priority_queue<pair<int, int>> pq;
     int j = 0;
     int ans = -1;
-    SegmentTree stree(n + 1, maxop);
+    SegmentTree stree(n + 1, 0, { SegmentTree::max, SegmentTree::assign });
     for (int i = 1; i <= n; ++i) {
         while (j < st.size() && st[j].first.first <= i) {
             pq.push(make_pair(-st[j].first.second, st[j].second));

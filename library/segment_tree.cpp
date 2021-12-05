50785f01adb47938784c9989cdf512cd475290a1
toysmars <toysmars@gmail.com>
2021-12-05 18:29:58 +0900

    Update segment_tree.cpp

--- library/segment_tree.cpp
+++ library/segment_tree.cpp
@@ -6,12 +6,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef int val_t;
-const val_t DEFAULT = 0;
+typedef long long i64;
+typedef i64 val_t;
 
+// Segment tree
+// The index of the first element should be 1.
 struct SegmentTree {
-    SegmentTree(int n, val_t (*op)(val_t, val_t)) : n(n), t(4 * n + 1, DEFAULT), op(op) {}
+    SegmentTree(int n, val_t def_val, val_t (*op)(val_t, val_t)) : n(n), def_val(def_val), t(4 * n + 1, def_val), op(op) {}
 
+    // Update the value of `i`th element to `x`.
     void update(int i, val_t x) {
         update(i, x, 1, 1, n + 1);
     }
@@ -29,12 +32,13 @@ struct SegmentTree {
         t[idx] = op(res1, res2);
         return t[idx];
     }
+    // Query the value elements in range [ql, qr).
     val_t query(int ql, int qr) {
         return query(ql, qr, 1, 1, n + 1);
     }
     val_t query(int ql, int qr, int idx, int l, int r) {
         if (l >= qr || r <= ql || l >= r) {
-            return DEFAULT;
+            return def_val;
         }
         if (ql <= l && qr >= r) {
             return t[idx];
@@ -47,8 +51,10 @@ struct SegmentTree {
 
     static val_t min(val_t a, val_t b) { return a < b ? a : b; }
     static val_t max(val_t a, val_t b) { return a > b ? a : b; }
+    static val_t sum(val_t a, val_t b) { return a + b; }
 
     int n;
+    val_t def_val;
     vector<val_t> t;
     val_t (*op)(val_t, val_t);
 };

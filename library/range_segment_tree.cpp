9bbf9d07084b372297fd3be5ab85666b5b6e277b
toysmars <toysmars@gmail.com>
2021-12-06 22:01:09 +0900

    Update range_segment_tree.cpp

--- library/range_segment_tree.cpp
+++ library/range_segment_tree.cpp
@@ -11,14 +11,15 @@ using namespace std;
 
 typedef int val_t;
 
-const val_t DEFAULT = 0;
-typedef val_t (*BiOperator)(val_t, val_t);
-typedef val_t (*SetOperator)(val_t, val_t);
-typedef val_t (*RepeatOperator)(val_t, int);
+struct Operators {
+    val_t (*combine)(val_t, val_t);
+    val_t (*lazy_combine)(val_t, val_t);
+    val_t (*update)(val_t, val_t);
+    val_t (*repeat)(val_t, int);
+};
 
 struct SegmentTree {
-    SegmentTree() {}
-    SegmentTree(int n, BiOperator op, SetOperator sop, RepeatOperator rop) : n(n), t(4 * n + 1, DEFAULT), lz(4 * n + 1, DEFAULT), op(op), sop(sop), rop(rop) {}
+    SegmentTree(int n, val_t init_val, val_t def_val, Operators ops) : n(n), init_val(init_val), def_val(def_val), t(4 * n + 1, init_val), lz(4 * n + 1, init_val), ops(ops) {}
 
     // update the segment tree with value x for range [ul, ur)
     void update(int ul, int ur, val_t x) {
@@ -37,7 +38,7 @@ struct SegmentTree {
         int len = r - l;
         val_t res1 = update(ul, ur, x, idx * 2 + 0, l, l + len / 2);
         val_t res2 = update(ul, ur, x, idx * 2 + 1, l + len / 2, r);
-        t[idx] = op(res1, res2);
+        t[idx] = ops.combine(res1, res2);
         return t[idx];
     }
 
@@ -47,7 +48,7 @@ struct SegmentTree {
     val_t query(int ql, int qr, int idx, int l, int r) {
         propagate(idx, l, r);
         if (l >= qr || r <= ql || l >= r) {
-            return DEFAULT;
+            return def_val;
         }
         if (ql <= l && r <= qr) {
             return t[idx];
@@ -55,42 +56,36 @@ struct SegmentTree {
         int len = r - l;
         val_t res1 = query(ql, qr, idx * 2 + 0, l, l + len / 2);
         val_t res2 = query(ql, qr, idx * 2 + 1, l + len / 2, r);
-        return op(res1, res2);
+        return ops.combine(res1, res2);
     }
 
     void propagate(int idx, int l, int r) {
-        if (lz[idx] != DEFAULT) {
+        if (lz[idx] != init_val) {
             int len = r - l;
-            t[idx] = sop(t[idx], rop(lz[idx], len));
+            t[idx] = ops.update(t[idx], ops.repeat(lz[idx], len));
             if (len > 1) {
-                lz[idx * 2] = op(lz[idx * 2], lz[idx]);
-                lz[idx * 2 + 1] = op(lz[idx * 2 + 1], lz[idx]);
+                lz[idx * 2] = ops.lazy_combine(lz[idx * 2], lz[idx]);
+                lz[idx * 2 + 1] = ops.lazy_combine(lz[idx * 2 + 1], lz[idx]);
             }
-            lz[idx] = DEFAULT;
+            lz[idx] = init_val;
         }
     }
 
     int n;
+    val_t init_val;
+    val_t def_val;
     vector<val_t> t;
     vector<val_t> lz;
-    BiOperator op;
-    SetOperator sop;
-    RepeatOperator rop;
+    Operators ops;
 
     static val_t min(val_t a, val_t b) { return a < b ? a : b; }
-    static val_t min_set(val_t a, val_t b) { return b; }
-    static val_t min_repeat(val_t a, int r) { return a; }
-
     static val_t max(val_t a, val_t b) { return a > b ? a : b; }
-    static val_t max_set(val_t a, val_t b) { return b; }
-    static val_t max_repeat(val_t a, int r) { return a; }
-
     static val_t add(val_t a, val_t b) { return a + b; }
-    static val_t add_set(val_t a, val_t b) { return a + b; }
-    static val_t add_repeat(val_t a, int r) { return r * a; }
-
-    static val_t xor_bi(val_t a, val_t b) { return a ^ b; }
-    static val_t xor_set(val_t a, val_t b) { return a ^ b; }
+    static val_t xorop(val_t a, val_t b) { return a ^ b; }
+    static val_t assign(val_t a, val_t b) { return b; }
+    static val_t no_repeat(val_t a, int r) { return a; }
+    static val_t times_repeat(val_t a, int r) { return r * a; }
     static val_t xor_repeat(val_t a, int r) { return (r & 1) ? a : 0; }
 };
+
 

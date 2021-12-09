af18f5c30ff1d6fbe12d096efb1dd7135dd3c69c
toysmars <toysmars@gmail.com>
2021-12-10 01:29:57 +0900

    Update 13974_knuth_opt.cpp

--- acmicpc/13974_knuth_opt.cpp
+++ acmicpc/13974_knuth_opt.cpp
@@ -6,8 +6,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long i64;
-
 // Knuth Optimization
 // Applicable when:
 // * C[a][c] + C[b][d] <= C[a][d] + C[b][c]
@@ -16,8 +14,11 @@ typedef long long i64;
 // * DP[i][j] = min(i < k < j) { dp[i][k] + dp[k][j] } + C[i][j]
 // * then range of k: K[i, j-1] <= K[i][j] <= K[i+1][j]
 const int MAX_SIZE = 5000;
-i64 dp[MAX_SIZE+1][MAX_SIZE+1];
-i64 mk[MAX_SIZE+1][MAX_SIZE+1];
+const int MAX_INT = numeric_limits<int>::max();
+int A[MAX_SIZE+1];
+int S[MAX_SIZE+1];
+int dp[MAX_SIZE+1][MAX_SIZE+1];
+int mk[MAX_SIZE+1][MAX_SIZE+1];
 
 int main() {
     int T;
@@ -25,8 +26,6 @@ int main() {
     while (T--) {
         int n;
         scanf("%d", &n);
-        vector<int> A(n);
-        vector<i64> S(n + 1);
         for (int i = 0; i < n; ++i) {
             scanf("%d", &A[i]);
             S[i + 1] = S[i] + A[i];
@@ -37,9 +36,9 @@ int main() {
         for (int w = 2; w <= n; ++w) {
             for (int i = 0; i + w <= n; ++i) {
                 int j = i + w;
-                dp[i][j] = numeric_limits<i64>::max() / 2;
+                dp[i][j] = MAX_INT;
                 for (int k = mk[i][j - 1]; k <= mk[i + 1][j]; ++k) {
-                    i64 v = dp[i][k] + dp[k][j] + S[j] - S[i];
+                    int v = dp[i][k] + dp[k][j] + S[j] - S[i];
                     if (dp[i][j] > v) {
                         dp[i][j] = v;
                         mk[i][j] = k;
@@ -47,7 +46,7 @@ int main() {
                 }
             }
         }
-        printf("%lld\n", dp[0][n]);
+        printf("%d\n", dp[0][n]);
     }
     return 0;
 }

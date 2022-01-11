ada51012f3565ab62425822285e8e104268162dc
toysmars <toysmars@gmail.com>
2022-01-11 23:11:16 +0900

    Update persistent_segment_tree.cpp

--- library/persistent_segment_tree.cpp
+++ library/persistent_segment_tree.cpp
@@ -2,6 +2,7 @@
 //
 // Examples:
 // * https://www.acmicpc.net/problem/11012
+// * https://www.acmicpc.net/problem/11932
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -24,6 +25,8 @@ struct Operations {
 // Persistent Segment tree
 // The index of the first element should be 1.
 struct PersistentSegmentTree {
+    // n: number of elements(size of the problem space).
+    // m: expected number of update operations.
     PersistentSegmentTree(int n, int m, val_t def_val, Operations ops, const vector<val_t>& init) : n(n), def_val(def_val), ops(ops) {
         nodes.reserve(4 * (n + m) + 1);
         root.reserve(m + 1);
@@ -49,8 +52,15 @@ struct PersistentSegmentTree {
 
     // Update the value of `i`th element to `x` by creating a new tree.
     // Returns the new segment tree creation time.
-    idx_t update(int i, val_t x) {
+    idx_t Update(int i, val_t x) {
         auto res = update(i, x, root[root.size() - 1], 1, n + 1);
+        root.push_back(res.first);
+        return root.size() - 1;
+    }
+    // Update the value of `i`th element to `x` by creating a new tree on a root created at time `t`.
+    // Returns the new segment tree creation time.
+    idx_t Update(int t, int i, val_t x) {
+        auto res = update(i, x, root[t], 1, n + 1);
         root.push_back(res.first);
         return root.size() - 1;
     }
@@ -74,7 +84,7 @@ struct PersistentSegmentTree {
     }
 
     // Query the value elements in range [`ql`, `qr`) from a segment tree created at time `t`.
-    val_t query(int t, int ql, int qr) {
+    val_t Query(int t, int ql, int qr) {
         return query(ql, qr, root[t], 1, n + 1);
     }
     val_t query(int ql, int qr, int idx, int l, int r) {
